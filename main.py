b36623159f5f68635d26be9fd0425739181ea3a5
Bruno Cabral <cabral.brc@gmail.com>
2022-02-11 21:42:33 -0800

    screen setup

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from settings import *
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.fill(BG_COLOR)
+
+    # drawing logic
+    pygame.display.update()
+    clock.tick(60)
