22c2ddb561b5bae79ba8a9011827c376ba198da2
Bruno Cabral <cabral.brc@gmail.com>
2022-02-11 21:43:26 -0800

    set title

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from settings import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Platformer')
 clock = pygame.time.Clock()
 
 while True:
