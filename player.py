f1597113c14259938fe982d98f108cd3e4ba4613
Bruno Cabral <cabral.brc@gmail.com>
2022-02-11 21:53:42 -0800

    player setup

---
+++ player.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos: Tuple, groups: List[pygame.sprite.Group]):
+        super().__init__(groups)
+        self.image = pygame.Surface((TILE_SIZE//2, TILE_SIZE))
+        self.image.fill(PLAYER_COLOR)
+        self.rect = self.image.get_rect(topleft = pos)
